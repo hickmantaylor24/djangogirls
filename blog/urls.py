3cc8ac8e914a5747a948826df56360693e724ed2
hickmantaylor24 <hickmantaylor24@gmail.com>
2024-05-05 14:02:29 -0400

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from . import views
+from django.contrib import admin
+from django.contrib.auth import views
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
-     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    
 ]
